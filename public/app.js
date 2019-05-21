d33d1370f6a7efb20e817aa4418b45f59770ce46
Paulo Henrique <gladgamer01@gmail.com>
2019-05-21 03:47:20 -0300

    Add files via upload

--- public/app.js
+++ public/app.js
@@ -1,11 +1,38 @@
 var socket = io.connect('http://localhost:80');
 
-socket.on("hello",(data)=>{
-	$('body').append('<video id="vid1" class="video-js vjs-default-skin" autoplay width="640" height="264" data-setup=\'{ "techOrder": ["youtube"], "sources": [{ "type": "video/youtube", "src": "' + data["src"] + '"}], "youtube": { "start": ' + data["seconds"] + ' } }\'  ></video>');
+var data_current = []
+myid = "";
+
+socket.emit("getMyId");
+socket.on("myId",(data)=>{$("#myid").text("My Id: " + data); myid=data;})
+
+socket.on("deletePlayerCurrent",()=>{
+	$("#vid1").remove();
+	socket.emit("deleteOk");
+});
+socket.on("currentVideo",(data)=>{
+	console.log(data);
+	if(data["id"] == socket.id){
+	$('#player').append('<video id="vid1" class="video-js vjs-default-skin" autoplay width="648" height="528" data-setup=\'{ "techOrder": ["youtube"], "sources": [{ "type": "video/youtube", "src": "' + data["data"]["src"] + '"}], "youtube": { "start": ' + data["data"]["seconds"] + ' } }\'  ></video>');
+	$("#ownerId").text(data["data"].owner);
+}
 });
 
+
 setInterval(()=>{
-	socket.emit("tick");
+	socket.emit("tick",data_current);
 },1000);
 
-socket.on("response",()=>{console.log("Tick!");});
+socket.on("response",(data)=>{
+	console.log(data['connections']);
+	$("#users").text("Users Connected (" + data['connections'] + ")");
+	$("#timerVideo").text( Math.floor(data["seconds_video"] / 60) + ":" + data["seconds_video"] + " /5minutos Restantes..");
+});
+
+socket.on("responseTick",()=>{socket.emit("tickUpdated")});
+var urlResult;
+$("#btnSendVideo").click(()=>{
+	 urlResult = $("#urlVideo").val();
+	 socket.emit("FilaAdd",{id:socket.id,url:urlResult});
+	 console.log(socket.id);
+});
