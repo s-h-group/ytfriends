f415b5d93e62ec7d844ad09a1ec8c1dd22433081
Paulo Henrique <gladgamer01@gmail.com>
2019-05-21 02:38:04 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -5,24 +5,58 @@ var server = app.listen(80,()=>{
 });
 var change = true;
 
+var server_info = {
+	connections: 0,
+	status: "playingvideo",
+	seconds_video: 0,
+	tick: false
+}
+
+var host = {
+	id: "",
+	selected: false,
+	data: {src:"",seconds:0,playing:false}
+}
+
 var current_video = {
-	src: "https://www.youtube.com/watch?v=60o1g1E2GBs",
+	src: "https://www.youtube.com/watch?v=UmtxtnP9AmA",
 	seconds: 0,
 	playing: true
 }
 
-
 var io = require('socket.io')(server);
 
+app.use(express.static('./public'));
+
 io.on("connection",(socket)=>{
+		server_info.connections = server_info.connections +1;
+		console.log(server_info);
 	if(current_video["playing"] == true){
-		io.emit("hello",current_video);
+		io.emit("currentVideo",current_video);
 	}
 socket.on("Reload",() => {
 console.log("got it");
 });
+
+socket.on("tickUpdated",()=>{server_info.tick = true; console.log("tick updated");});
+socket.on("disconnect",()=>{server_info.connections = server_info.connections -1;})
 socket.on("tick",()=>{
-	io.emit("response");
-	current_video["seconds"] +=1;
+	if(server_info.tick == false){
+		io.emit("responseTick");
+	}
+	if(server_info.status == "choosenewvideo"){
+
+	}
+	if(current_video["seconds"]>=300){
+		current_video.playing = false;
+		server_info.status = "choosenewvideo";
+	}else{
+		if(server_info.tick == true){
+			current_video["seconds"] +=1;
+		  server_info.seconds_video = current_video["seconds"];
+			server_info.tick = false;
+		}
+}
+	io.emit("response",server_info);
 });
 });
